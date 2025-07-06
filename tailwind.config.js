9d70f5a255de9247a0c1147585cb8e8d56a6356e
msislam2308 <msislam2308@gmail.com>
2025-07-06 23:58:30 +0600

    alhamdulliah good

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/**
+ * tailwing.json.js
+ * Configuration for Tailwind CSS in JavaScript format.
+ */
+
+export default {
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx,html}",
+        "./public/index.html"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
